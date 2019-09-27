5db15f7356b18ef6be2f465786336730d48268b5
Coldairarrow <xgc15657519397@outlook.com>
2019-09-27 17:28:36 +0800

    更新

---
+++ src/TaskSchedule/Job/ProjectNoticeJob.cs
@@ -0,0 +1,68 @@
+﻿using Coldairarrow.Business;
+using Coldairarrow.Business.BusManage;
+using Coldairarrow.DataRepository;
+using Coldairarrow.Entity.BusManage;
+using Coldairarrow.Util;
+using Quartz;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskSchedule
+{
+    class ProjectNoticeJob : IJob
+    {
+        IRepository _db { get; } = DbFactory.GetRepository();
+        MsgHelper _msgHelper { get; } = new MsgHelper();
+        Bus_ProjectBusiness _proBus { get; } = new Bus_ProjectBusiness();
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Task.Run(() =>
+            {
+                //待分配或审核通知
+                if (_db.GetIQueryable<Bus_Project>()
+                    .Any(x => x.IsDeleted == false && (x.NiCheState == 0 || x.NiCheState == 1)))
+                {
+                    _msgHelper.SendAuditNotice("18705808736");
+                }
+
+                //超时未跟踪通知
+                //超时1星期
+                DateTime endTime = DateTime.Now.AddDays(-7);
+                var projectList = _proBus
+                    .GetDataList(true, null, null, null, null, 2, null, null, endTime, null, null,null, new Pagination())
+                    .ToList();
+
+                projectList.ForEach(aProject =>
+                {
+                    try
+                    {
+                        _msgHelper.SendTrackNotice(aProject.MarketerPhoneNum, aProject.ProjectName, 1);
+                    }
+                    catch
+                    {
+
+                    }
+                });
+
+                //超时2星期
+                endTime = DateTime.Now.AddDays(-14);
+                projectList = _proBus
+                    .GetDataList(true, null, null, null, null, 2, null, null, endTime,null,null,null, new Pagination())
+                    .ToList();
+
+                projectList.ForEach(aProject =>
+                {
+                    try
+                    {
+                        _msgHelper.SendTrackNotice(aProject.MarketingManagerPhoneNum, aProject.ProjectName, 2);
+                    }
+                    catch
+                    {
+
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ src/TaskSchedule/Program.cs
@@ -0,0 +1,107 @@
+﻿using Coldairarrow.Util;
+using Coldairarrow.Util.WindowsService;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace TaskSchedule
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            WindowsServiceContainer container = new WindowsServiceContainer("ccs_opportunity_mgr_TaskSchedule", args)
+            {
+                Description = "商机管理系统定时任务服务",
+                HandleOnStart = argss =>
+                {
+                    LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+                    RunScheduler().GetAwaiter().GetResult();
+                }
+            };
+
+            container.Start();
+
+            //LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+            //RunScheduler().GetAwaiter().GetResult();
+
+            //Console.ReadLine();
+        }
+
+        private static async Task RunScheduler()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                IScheduler scheduler = await factory.GetScheduler();
+
+                await scheduler.Start();
+
+                IJobDetail job = JobBuilder.Create<ProjectNoticeJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithCronSchedule("0 30 8 * * ?")//每天8.30
+                    .Build();
+
+                await scheduler.ScheduleJob(job, trigger);
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+        }
+
+        private class ConsoleLogProvider : ILogProvider
+        {
+            public Logger GetLogger(string name)
+            {
+                return (level, func, exception, parameters) =>
+                {
+                    if (level >= LogLevel.Info && func != null)
+                    {
+                        //string msg = "[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters;
+                        //FileHelper.WriteLog(msg)
+                        Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters);
+                    }
+                    return true;
+                };
+            }
+
+            public IDisposable OpenNestedContext(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IDisposable OpenMappedContext(string key, string value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    public class HelloJob : IJob
+    {
+        public HelloJob()
+        {
+            Console.WriteLine("新建对象");
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync($"{DateTime.Now}:Greetings from HelloJob!");
+        }
+    }
+}
